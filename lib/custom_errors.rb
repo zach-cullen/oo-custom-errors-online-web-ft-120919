e9df41fba1808f6c27781a6524f3f79f09b55d00
Zach Cullen <zacullen@gmail.com>
2020-01-08 16:53:01 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,7 +7,7 @@ class Person
 
   def get_married(person)
     self.partner = person
-    if person.class @= Person
+    if person.class != Person
       raise PartnerError
     else 
       person.partner = self
